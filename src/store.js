da21a11601fa8039aeb0d4d6b5ff06a31bee0080
l1f21bsse0147 <farazahmad9887@gmail.com>
2025-03-22 05:55:07 +0500

    file modified  completed

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './redux/cartSlice'
 export const store = configureStore({
   reducer: {
-   cart:cartReducer
+   cart:cartReducer,
   },
 })
